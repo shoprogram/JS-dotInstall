f73dd45975f9660734a061d937fca2a403a255d6
shoprogram <shoprogramsa@gmail.com>
2021-04-11 20:10:23 +0900

    study:add

--- JavaScriptBC/js/main.js
+++ JavaScriptBC/js/main.js
@@ -4,4 +4,49 @@ const scores = [60, 90 , 30, 70]
 
 scores.forEach((score, index) => {
   console.log(`Score${index}: ${score}`)
+  console.log(score + 30)
 })
+
+const updateScore = scores.map((score) => {
+  return score + 30;
+})
+console.log(updateScore);
+
+const newScore = scores.filter((score) => {
+ return score % 2 === 0;
+})
+console.log(newScore);
+
+const point = {
+  x: 100,
+  y: 180,
+}
+console.log(point)
+
+point.z = 200;
+console.log(point)
+
+delete point.y
+console.log(point)
+
+//forEachはオブジェクトにそのまま使えない
+// point.forEach((value) => {
+//   console.log(value)
+// })
+
+//なのでObject.keysでオブジェクトのすべてのキーと取得できることを利用し、keyを使って展開していく
+
+const keys = Object.keys(point);
+  keys.forEach(key => {
+    console.log(key, point[key])
+  })
+
+
+//変数でスプレット構文を使うと、代入する前の値が取り出せる
+let x = [1, 2];
+let y = [...x];
+let z = x;
+x[0] = 5;
+console.log(x)
+console.log(y)
+console.log(z)
