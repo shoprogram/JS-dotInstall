5f7262700a2469dfd2567ab321e8cb6b111d0b5d
shoprogram <shoprogramsa@gmail.com>
2021-04-20 16:20:50 +0900

    study: add

--- JavaScriptDOM/JS/main.js
+++ JavaScriptDOM/JS/main.js
@@ -56,5 +56,91 @@
     const item3 = document.querySelectorAll('li')[2];
     ul.insertBefore(cloneItem, item3);
   }) 
+
+  document.getElementById('delete').addEventListener('click', () => {
+    const item2  = document.querySelectorAll('li')[1]
+
+    // item2.remove();
+    // ↑これでも削除できるが、使えないブラウザがあるで、その場合は
+    // 親Node.removeChild(削除するNode)
+    // を使うことで対応可↓
+    document.querySelector('ul').removeChild(item2);
+  })
+
+  document.getElementById('addButton').addEventListener('click', () => {
+    const li = document.createElement('li');
+    const inputText = document.querySelector('input');
+    // .valueにinputしたテキストが入る
+    li.textContent = inputText.value;
+    document.getElementById('list').appendChild(li);
+
+    //appendChildでテキストを追加した後.valueの中身をからにし、inputフォームをfocusさせる
+    inputText.value = "";
+    inputText.focus();
+  })
   
+  document.getElementById('radio').addEventListener('click', ()=>{
+    const colors = document.querySelectorAll('input')
+    // console.log(colors);
+    let selectedColor;
+    
+    console.log(colors)
+    colors.forEach(color => {
+      if(color.checked === true) {
+        //radioボタンのチェックが付いている要素を判定する
+        selectedColor = color.value;
+      }
+    })
+    
+    const li = document.createElement('li');
+    li.textContent = selectedColor;
+    document.getElementById('radioList').appendChild(li);
+  })
+
+  document.getElementById('event').addEventListener('dblclick',() => {
+    console.log('double clicked!');
+    //ダブルクリックで発火
+  })
+
+  // document.addEventListener('mousemove', e => {
+  //   //マウスを動かした時点で発火
+  //   console.log(e.clientX, e.clientY);
+  //   //引数にeを取り、e.clientX, e.clientYで画面内の座標を獲得できる
+  // })
+
+  // document.addEventListener('keydown', e => {
+  //   console.log(e.key)
+  //   //押したキーを判定できる。
+  // })
+
+
+  const area = document.getElementById('area');
+
+  area.addEventListener('focus', () => {
+    console.log('focus');
+  })
+  
+  area.addEventListener('blur', () => {
+    console.log('blur');
+    //focusが外れたときに発火する。(focusと逆の効果)
+  })
+
+  area.addEventListener('input', () => {
+    console.log('input');
+    //内容が更新されたときに発火
+
+    console.log(area.value.length);
+    //文字数を判定することも可能
+  })
+  
+  area.addEventListener('change', () => {
+    console.log('change');
+    //内容が確定したときに発火
+  })
+  
+  document.querySelector('form').addEventListener('submit', e  => {
+    e.preventDefault();
+    //通常formを送信するとページがリロードされるので、それを回避するためにe.preventDefaultを使う
+    console.log('sibmit');
+  })
 }
